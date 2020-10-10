ad24205fb52ba31a379a0594c42d7e5e59741ce0
dahaliahowell <dahaliahowell.dh@gmail.com>
2020-10-09 22:43:47 -0500

    Added functionality to the squares

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,51 @@
 "use strict";
 
-//let squares = document.querySelectorAll('.square');
-window.onload = function(){
-    let divs = document.querySelectorAll('#board > div');
-    console.log(divs.length)
+var squares = [];
+var gameTracker = new Array(9);
 
-    divs.forEach(function(element, index, list) {
+window.onload = function(){
+
+    this.squares = document.querySelectorAll('#board > div');
+    console.log(squares.length)
+
+    squares.forEach(function(element, index, list) {
         element.classList.add("square");
         console.log('added square!');
     })
+
+    this.selector()
+
+}
+
+function selector() {
+
+    squares.forEach(function(element, index, list) {
+
+        element.addEventListener('click', function(e) {
+
+            if (e.target.innerHTML == 'O') {
+                e.target.classList.remove('O');
+                e.target.classList.add('X');
+                e.target.innerHTML = 'X';
+                gameTracker[index] = 'X'
+                console.log('click x')
+
+            } else if (e.target.innerHTML == 'X') {
+                e.target.classList.remove('X');
+                e.target.classList.add('O');
+                e.target.innerHTML = 'O';
+                gameTracker[index] = 'O'
+                console.log('click o')
+
+            } else {
+                e.target.classList.add('X');
+                e.target.innerHTML = 'X';
+                gameTracker[index] = 'X'
+                console.log('click x')
+            }
+        })
+
+    }
+
+    )
 }
